0d366e441c5ac2ad401dd268392d08c4881a0a7c
Issa Nyale <me@email.com>
2022-11-28 14:44:04 +0300

    Created index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+speedlimit();
+console.log(speedlimit = 70);
+return speedlimit;
+
+function speedlimit(){
+   let speedlimit = '70'
+   if (speedlimit < 70){
+    print = "Ok";
+   }
+
+   let newspeedlimit = 'per every 5km/s above 70'
+   function speedlimit(){
+    let newspeedlimit = '75'
+    if (newspeedlimit = 75){
+        print = "points 1"
+    }
+    else if (newspeedlimit = 80){
+        print = "points 2"
+    }
+    else if (newspeedlimit = 85){
+    print = "points 3"
+    }
+    else if (newspeedlimit = 90){
+        print = "points 4"
+    }
+    else if (newspeedlimit = 95){
+        print = "points 5"
+    }
+    else if (newspeedlimit = 100){
+        print = "points 6"
+    }
+    else if(newspeedlimit = 105){
+        print = "points 7"
+    }
+    else if(newspeedlimit = 110){
+        print = "points 8"
+    }
+    else if(newspeedlimit = 115){
+        print = "points 9"
+    }
+    else if(newspeedlimit = 120){
+        print = "points 10"
+    }
+    else if(newspeedlimit = 125){
+        print = "points 11"
+    }
+    else if(newspeedlimit = 130){
+        print = "points 12"
+    }
+    else if(newspeedlimit > 135){
+        print = "License suspended"
+    }
+   } 
+}
